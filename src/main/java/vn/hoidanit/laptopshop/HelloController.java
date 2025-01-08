fcb19b1f16237769b7ade661ada348ed3b17cd9c
Rawuy Nguyen <huybguyen881@gmail.com>
2025-01-08 13:46:31 +0700

    kkk

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -2,14 +2,12 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Xin chào cả nhà yêu hé";
+        return "Xin chào cả nhà chào";
     }
 }
