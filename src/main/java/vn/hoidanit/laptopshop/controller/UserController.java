c0028c63d9620305eab76cf1989af34e1de37c14
Rawuy Nguyen <huybguyen881@gmail.com>
2025-02-08 16:16:35 +0700

    Repository query

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,38 +1,46 @@
 package vn.hoidanit.laptopshop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.ui.Model;
 
 import vn.hoidanit.laptopshop.service.UserService;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 
 @Controller
 public class UserController {
     // DI: dependency injection
-    private UserService userService = null;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
-        String text = this.userService.handleHello();
+    public String getHomePage(Model model) {
+        List<User> arrUsers = this.userService.getAllUsersByEmail("2@gmail.com");
+        System.out.println(arrUsers);
+        model.addAttribute("text", "text");
+        model.addAttribute("name", "from controller with model");
         return "hello";
     }
 
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model mode, @ModelAttribute("newUser") User hoidanIT) {
+        System.out.println("run here" + hoidanIT);
+        this.userService.handleSaveUser(hoidanIT);
+        return "hello";
+    }
 }
-
-// @RestController
-// public class UserController {
-// // DI: dependency injection
-// private UserService userService = null;
-
-// public UserController(UserService userService) {
-// this.userService = userService;
-// }
-
-// @GetMapping("/")
-// public String getHomePage() {
-// return this.userService.handleHello();
-// }
-// }
--- src/main/webapp/WEB-INF/view/hello.jsp
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -1,11 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Hello from JSP!
-</body>
-</html>
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+
+        <body>
+            Hello from JSP!
+            <h1>
+                ${text}
+            </h1>
+            <h2> ${name}</h2>
+            <h3>${helloae}</h3>
+        </body>
+
+        </html>
